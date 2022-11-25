a1b9551282319cb1ea2b52254633a6ecfe462dd1
fafas <fafachamani@gmail.com>
2022-11-25 14:32:37 +0330

    changes gradient background login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 
-import './App.css';
+//import './App.css';
 //import Store from './store/Store';
-import Header from './header/Header';
+//import Header from './header/Header';
+import Loogin from "./log in/Loogin";
 
 function App() {
 
 //crate store
 //import in app.js
-//obj in app.js
+//obj in app.js//
 //in store.js=>map=>obj
 //install tailwindcss
 //style tailwind
 
 
   return (
-    
+    <Loogin/>
   );
 }
 
---
+++ src/log in/Loogin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const Loogin = () => {
+  return (
+    <div className='bg-red-300 h-screen flex flex-row justify-center item-center  bg-gradient-to-br from-lime-500 via-lime-300 to-yellow-300' >
+        <div className=' bg-red-500'>
+salam
+        </div>
+    </div>
+  )
+}
+export default Loogin;
