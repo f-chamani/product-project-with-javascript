0b3a4fe45ee99c5a3c9567133d823085b6eedff2
fafas <fafachamani@gmail.com>
2022-11-27 08:06:22 +0330

    update Third login

--- src/log in/Loogin.js
+++ src/log in/Loogin.js
@@ -1,27 +1,26 @@
 import React from 'react'
+import back from "../assets/images/logo.jpg"
+
 
  const Loogin = () => {
   return (
-    <div className='bg-red-300 h-screen flex flex-row  justify-center item-center bg-gradient-to-br from-lime-500 via-lime-300 to-yellow-300 ' >
-        <div className=' bg-white grid grid-cols-2 rounded-2xl '>
 
-              <div className=''>
-              
-                  
-
-              </div>
-              <div className=' flex flex-col gap-3 text-gray-700'>
-
-              <h1 className="font-bold text-2xl">User login</h1>
-              <input  type="text" className='bg-gray-100 rounded-xl px-2 py-3' placeholder='نام کاربری' />
-              <input  type="text" className='bg-gray-100 rounded-xl px-2 py-3' placeholder= 'رمز عبور' />
-              <button className='bg-gradient-to-r from-lime-500 via-lime-300 to-yellow-300  rounded-xl'> ورود</button>
-              <label className='text-slate-500 text-xs'>رمز عبور را فراموش کرده اید؟</label>
-              <label className='text-slate-500 text-xs'>ساختن عضو جدید -> </label>
-
-              </div>
-
+    <div className='h-screen bg-gradient-to-br from-lime-500 via-lime-300 to-yellow-300 flex item-center  justify-center '>
+      <div className='' >
+        <div className='bg-white grid grid-cols-2 rounded-2xl overflow-hidden '>
+            <div className=' flex flex-col gap-3 text-gray-700'>
+                <h1 className="font-bold text-2xl">User login</h1>
+                <input  type="text" className='bg-gray-100 rounded-xl px-2 py-3' placeholder='نام کاربری' />
+                <input  type="text" className='bg-gray-100 rounded-xl px-2 py-3' placeholder= 'رمز عبور' />
+                <button className='bg-gradient-to-r from-lime-500 via-lime-300 to-yellow-300  rounded-xl'> ورود</button>
+                <label className='text-slate-500 text-xs'>رمز عبور را فراموش کرده اید؟</label>
+                <label className='text-slate-500 text-xs'>ساختن عضو جدید -{'>'} </label>
+            </div>
+             <div>
+               <img src={back} />   
+             </div>
         </div>
+    </div>
     </div>
   )
 }
