a61a6081bc393ac4cf59ea69b1da2df56bdd9893
fafas <fafachamani@gmail.com>
2022-11-28 10:40:27 +0330

    center object in page login

--- src/log in/Loogin.js
+++ src/log in/Loogin.js
@@ -5,8 +5,8 @@ import back from "../assets/images/logo.jpg"
  const Loogin = () => {
   return (
 
-    <div className='h-screen flex justify-center  justify-items-center bg-gradient-to-br from-lime-500 via-lime-300 to-yellow-300   '>
-      <div className='' >
+    <div className='  flex flex-row justify-center items-center  h-screen bg-gradient-to-br from-lime-500 via-lime-300 to-yellow-300   '>
+      
         <div className='bg-white grid grid-cols-2 rounded-2xl overflow-hidden '>
             <div className=' flex flex-col gap-3 text-gray-700'>
                 <h1 className="font-bold text-2xl">User login</h1>
@@ -20,7 +20,7 @@ import back from "../assets/images/logo.jpg"
                <img src={back} />   
              </div>
         </div>
-    </div>
+   
     </div>
   )
 }
