9c7e9c8983205ba29ea9ead1d79b85027e3ea938
fafas <fafachamani@gmail.com>
2022-12-02 21:23:04 +0330

    update 1401/9/11 9.22 PM

--- src/book/Book.js
+++ src/book/Book.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Book() {
   return (
-    <div>Book</div>
+    <div>
+        <div className='  flex flex-row items-center justify-center flex justify-between gap-2  border-b-2 '>
+            <div className="flex flex-row gap-4 ">
+                <div className=" hover:bg-amber-100 gap-1 py-2 ">صحفه اصلی </div>
+                <div className=" hover:bg-amber-100 gap-1  py-2">اشخاص </div>
+                <div className=" hover:bg-amber-100 gap-1  py-2" >تعاریف دانشگاه ها </div>
+                <div className=" hover:bg-amber-100 gap-1  py-2">درباره ی ما </div>
+                <div className=" hover:bg-amber-100 gap-1  py-2">تماس با ما </div>
+            </div>
+            <div  className="flex flex-row gap-4 pl-4">
+                <div className=" hover:bg-amber-100  gap-1  py-2 ">ورود </div>
+                <div className=" hover:bg-amber-100  gap-1  py-2 ">ثبت نام </div>
+            </div>
+
+        </div>
+    </div>
   )
 }
