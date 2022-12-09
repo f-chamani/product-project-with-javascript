4dc5163511644ee7b5826831f29285c338372bd2
fafas <fafachamani@gmail.com>
2022-12-09 12:41:16 +0330

    update 12/9/2022 12:40PM  (MOVIES )

--- src/book/Book.js
+++ src/book/Book.js
@@ -3,6 +3,10 @@ import {TbMovie} from 'react-icons/tb';
 import {AiFillClockCircle} from 'react-icons/ai';
 import {ImStop2} from 'react-icons/im';
 import {ImSearch} from 'react-icons/im';
+import {FiShare2} from 'react-icons/fi';
+import {HiPencilAlt} from 'react-icons/hi';
+import {FiMessageSquare} from 'react-icons/fi';
+
 
 
 export default function Book() {
@@ -23,9 +27,9 @@ export default function Book() {
                     </div>
                 </div>
                 <div className=" h-44"></div>
-                <div className="grid grid-cols-6 gap-8  py-16">
+                <div className="grid grid-cols-6 gap-24  py-16">
                     <div className=" flex flex-col col-span-2 gap-8">
-                        <div className="flex flex-col gap-6 bg-gray-200  rounded-2xl p-8  relative">
+                        <div className="flex flex-col gap-12 bg-gray-200  rounded-2xl p-8  relative">
                             <div className="absolute right-1/2 transform translate-x-1/2 -translate-y-1/3 top-0 rounded-2xl h-52 w-44 bg-center shadow-2xl border bg-cover  bg-[url('http://localhost:3000/assests/images/12.jpg')]"></div>
                             <div className="flex flex-col justify-center items-center pt-32">
                                 <h1 className="font-bold text-xl">چشم و گوش بسته</h1>
@@ -86,9 +90,17 @@ export default function Book() {
                             <input type="text" className=" bg-fuchsia-300 w-full px-9 border-2 py-2 rounded-3xl bg-transparent pr-14  "/>
 
                         </div>
-                        <div className="flex flex-col gap-6 bg-gray-200  rounded-2xl p-10 ">
+                        <div className="flex flex-col gap-6 bg-gray-200  rounded-xl p-10 relative ">
+                            <div className="bg-purple-700 w-64 pr-14 rounded-tl-xl  rounded-bl-xl translate-x-20 text-2xl text-amber-50 shadow-2xl text-2xl p-2 ">
+                                چشم و گوش بسته
+                            </div>
+                            <div className="w-0 h-0 absolute transform translate-x-20 translate-y-4
+                               border-t-[31px] border-t-transparent
+                               border-l-[40px] border-l-purple-700
+                               border-b-[31px] border-b-transparent">
+                            </div>
                             <div className="flex flex-col justify-center items-center">
-                                <p className="text-2xl">
+                                <p className="text-xl">
                                     چشم و گوش بسته فیلمی به کارگردانی فرزاد مؤتمن، نویسندگی هما بذرافشان و تهیه‌کنندگی
                                     امیرحسین آشتیانی‌پور محصول سال ۱۳۹۷ و ١٣٩٨ است. این فیلم در شهریور ۱۳۹۸ از شورای
                                     پروانه نمایش فیلم‌های سینمایی مجوز نمایش گرفت.[۱]
@@ -100,55 +112,59 @@ export default function Book() {
                                     دارند که از این مهلکه جان سالم به در ببرند.
                                 </p>
                             </div>
+                            <div className="flex flex-row-reverse gap-2 border-solid border-solid border-t-2 pt-5 border-indigo-900 ">
+                                <FiShare2 size={24}/>
+                                <HiPencilAlt size={24}/>
+                                <FiMessageSquare size={24}/>
+                            </div>
                         </div>
                         <div className=" flex flex-col p-8 gap-4  bg-gray-200 rounded-2xl ">
                             <div className="flex flex-row  ">
-                            <TbMovie size={25}/>
-                               <label> عوامل فیلم</label>
+                                <TbMovie size={25}/>
+                                <label> عوامل فیلم</label>
                             </div>
-                           <div className=" grid grid-cols-6 flex gap-x-4 gap-y-4">
+                            <div className=" grid grid-cols-6 flex gap-x-4 gap-y-4">
 
-                               <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
-                                   <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
-                                       <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
-                                   </div>
-                                   <div className="text-center py-2 px-0.5">
-                                       <span>هادی حجازی فرد</span>
-                                   </div>
-                               </div>
+                                <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
+                                    <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
+                                        <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
+                                    </div>
+                                    <div className="text-center py-2 px-0.5">
+                                        <span>هادی حجازی فرد</span>
+                                    </div>
+                                </div>
 
-                               <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
-                                   <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
-                                       <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
-                                   </div>
-                                   <div className="text-center py-2 px-0.5">
-                                       <span>هادی حجازی فرد</span>
-                                   </div>
-                               </div>
-
-
-                               <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
-                                   <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
-                                       <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
-                                   </div>
-                                   <div className="text-center py-2 px-0.5">
-                                       <span>هادی حجازی فرد</span>
-                                   </div>
-                               </div>
+                                <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
+                                    <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
+                                        <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
+                                    </div>
+                                    <div className="text-center py-2 px-0.5">
+                                        <span>هادی حجازی فرد</span>
+                                    </div>
+                                </div>
 
 
-                               <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
-                                   <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
-                                       <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
-                                   </div>
-                                   <div className="text-center py-2 px-0.5">
-                                       <span>هادی حجازی فرد</span>
-                                   </div>
-                               </div>
+                                <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
+                                    <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
+                                        <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
+                                    </div>
+                                    <div className="text-center py-2 px-0.5">
+                                        <span>هادی حجازی فرد</span>
+                                    </div>
+                                </div>
 
 
+                                <div className="flex flex-col items-center shadow-2xl row-span-1 overflow-hidden bg-green-100 rounded-2xl ">
+                                    <div className=" bg-center rounded-bl-2xl overflow-hidden   ">
+                                        <img className="" src="http://localhost:3000/assests/images/hadi.jpg" alt=""/>
+                                    </div>
+                                    <div className="text-center py-2 px-0.5">
+                                        <span>هادی حجازی فرد</span>
+                                    </div>
+                                </div>
 
-                           </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
